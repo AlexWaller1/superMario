889ed93bf5bc3a5978e421835231ad5426873b15
Alex Waller <alex@artofalexwaller.com>
2021-11-18 15:00:50 -0500

    loadSprite()

--- superMario.js
+++ superMario.js
@@ -13,6 +13,14 @@ loadSprite("coin", "wbKxhcd.png");
 loadSprite("evil-shroom", "KPO3fR9.png");
 loadSprite("brick", "pogC9x5.png");
 loadSprite("block", "bdrLpi6.png");
+loadSprite("mario", "Wb1qfhK.png");
+loadSprite("mushroom", "0wMd92p.png");
+loadSprite("surprise", "gesQ1KP.png");
+loadSprite("unboxed", "bdrLpi6.png");
+loadSprite("pipe-top-left", "ReTPiWY.png");
+loadSprite("pipe-top-right", "hj2GK4n.png");
+loadSprite("pipe-bottom-left", "c1cYSbt.png");
+loadSprite("pipe-bottom-right", "nqQ79ei.png");
 
 screen("game", () => {
   layers(["bg", "obj", "ui"], "obj");
